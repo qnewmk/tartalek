aa3b0922483824781c6b6263211a662b1f8be200
kaio hursan <kaioh@pop-os.localdomain>
2019-05-27 17:27:47 -0300

    primeiro commit

---
+++ app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/display')
+def display():
+    return render_template('display.html')
+if __name__ == '__main__':
+    app.run(debug=True)
